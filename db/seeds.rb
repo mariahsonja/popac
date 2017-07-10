447ccb9eb88ab6bb731b5eeda138a5c845fc55d6
Mariah <mariahsonja@hotmail.com>
2017-07-10 14:11:26 +0000

    Adding countries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 [
-    "Brazil",
-    "Ireland"
+    "Albania", "Andorra", "Armenia", "Austria", "Azerbaijan", "Belarus", "Belgium", "Bosnia and Herzegovina",
+    "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France",
+    "Georgia", "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Kazakhstan", "Kosovo",
+    "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia (FYROM)", "Malta", "Moldova",
+    "Monaco", "Montenegro", "Netherlands", "Norway", "Poland", "Portugal", "Romania", "Russia", "San Marino",
+    "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Turkey", "Ukraine", "United Kingdom (UK)"
 ].each do |country_name|
     puts "Creating Country #{country_name}"
     Country.where(name: country_name).first_or_create!
