5c44980c777391e21d420e37b0446a3f7bdefa90
Mariah <mariahsonja@hotmail.com>
2017-07-12 18:43:03 +0000

    fix profile form

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,11 @@ class Profile < ApplicationRecord
   belongs_to :country 
   has_and_belongs_to_many :interests 
   #MARIAH
-  accepts_nested_attributes_for :interests 
+  accepts_nested_attributes_for :interests
+  
+  validates :name, presence: true
+  validates :bio, presence: true
+  validates :interests, presence: true
   
   #MARIAH: serach  
    def self.search(search)
