acba4172af85f9cc4b1d3f969d1dc181bb745724
Mariah <mariahsonja@hotmail.com>
2017-07-13 21:05:35 +0000

    remove unused views

--- app/views/interests/_interest.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! interest, :id, :name, :created_at, :updated_at
-json.url interest_url(interest, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,16 @@
 Rails.application.routes.draw do
-  resources :interests
-  #MARIAH
-  root to: "profiles#show"
-  
-  resource :profile
-  
-  get :search, to: 'search#index'
-  
   devise_for :admins
   devise_for :users
   
+  # MARIAH
+  root to: "profiles#show"
   
-  #MARIAH
-  get '/about' => 'site#about'
-  #MARIAH
-  get '/contact' => 'site#contact'
-  #MARIAH
-  get '/Admin' => 'user#admin_login'
+  resources :interests
+  resource :profile
+  
+  get :search, to: 'search#index'
 
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #MARIAH
+  get '/about', to: 'site#about'
+  get '/contact', to: 'site#contact'
 end
